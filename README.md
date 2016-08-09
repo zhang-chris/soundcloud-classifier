0d26faa7a42cf3410b286e1aa5d399406288ed03
Bryan Yan <bryanyan@andrew.cmu.edu>
2016-08-09 00:20:07 -0700

    get song names and update README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# soundcloud-classifier
+# LazyList
 A web app that classifies an existing Soundcloud playlist and sorts the songs by genres into new playlists
+
+### TODO
+* Display page with playlist after authentication
+* Run songs through classification algorithm
+* Display classification results, ask if user would like to create new playlists with results
+* Create and display new playlists
--- app/views.py
+++ app/views.py
@@ -14,4 +14,15 @@ def authSoundCloud(request):
 	code = request.GET.get('code')
 	accessToken = soundcloudAPI.exchangeToken(code)
 	newClient = soundcloudAPI.userInfo(accessToken)
-	print newClient.get('/me').username
+	# TODO: Need to display favorites playlist, and then loop through songs
+	# in playlist and run song through classification algorithm
+	# Gives you name of songs in favorites
+	favorites = newClient.get('/me/favorites')
+	for song in favorites:
+		for key, value in song.fields().items():
+			if key == 'title':
+				print value
+
+
+def classification(song):
+	pass
